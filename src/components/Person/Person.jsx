11c96fea637584a6698b9761ddddd2ee640e2a55
Kostya Vasilenko <kostyantin.vasilenko.03@gmail.com>
2024-10-19 19:53:45 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import '../../App.scss';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <div className="Person">
+      <p className="Person__name">My name is {name}.</p>
+      <p className="Person__age">I am {age} years old.</p>
+      {isMarried ? (
+        <p className="Person__partner">
+          My {sex === 'm' ? 'wife' : 'husband'} is {partnerName}.
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married.</p>
+      )}
+    </div>
+  );
+};
